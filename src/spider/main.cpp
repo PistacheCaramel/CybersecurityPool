29253dfc266fe012d942ae79c2b2e004c6565233
Yohan BENDAVID <ybendavi@made-f0Cr6s3.clusters.42paris.fr>
2023-10-18 14:54:58 +0200

    extensions protected

--- src/spider/main.cpp
+++ src/spider/main.cpp
@@ -42,6 +42,21 @@ int   link_handler(const char *url, int lvl)
 
 }
 
+int	check_extension(std::string to_check)
+{
+	std::string sub;
+	
+	sub = to_check.substr(to_check.size() - 5, 5);
+	if (sub.compare(".jpeg") == 0)
+		return (1);
+	sub.erase(0, 1);
+	if (sub.compare(".jpg") == 0 || sub.compare(".png") == 0
+			|| sub.compare(".gif") == 0
+			|| sub.compare(".bmp") == 0)
+		return (1);
+	return (0);
+}
+
 int	img_request(const char *path)
 {
    	std::vector<std::string>::iterator img;
@@ -49,12 +64,66 @@ int	img_request(const char *path)
 	img = imgs.begin();
 	while (img != imgs.end())
 	{
-		if (get_image_data_from_url((*img).c_str(), path))
-			return (1);
+		if (check_extension(*img))
+		{
+			if (get_image_data_from_url((*img).c_str(), path))
+				return (1);
+		}
 		img++;
-
 	}
 	return (0);
+}
+
+void	recursive_option(std::string::iterator *it, std::string **input)
+{			
+	levl = 5;
+	*it += 2;
+	if (*it + 1  != (*input)->end() 
+			&& *(*it + 1) == '-' && *it + 2 != (*input)->end()
+			&& *(*it + 2) == 'l' && *it + 3 != (*input)->end()
+			&& *(*it + 3) == ' ' && *it + 4 != (*input)->end()
+			&& isdigit(*(*it + 4)))
+	{
+		*it += 4;
+		levl = 0;
+		while (*it != (*input)->end() && isdigit(**it))
+		{
+			std::cout << "HERE" << std::endl;
+			int	power = 0;
+			levl = levl * pow(10, power)  + (**it) - 48;
+			power++;
+			(*it)++;
+		}
+	}
+
+}
+void	options_handler(std::string *input, std::string *path)
+{
+	std::string::iterator	it;
+	it = input->begin();
+	while (it != input->end() && *it == '-')
+	{
+		if (*(it + 1) == 'r' && *(it + 2) == ' ')
+		{
+			recursive_option(&it, &input);	
+		}
+		else if (it + 1 != input->end() && *(it + 1) == 'p'
+				&& it + 2 != input->end())
+		{
+			size_t	pos;
+			it += 3;
+			pos = std::distance(input->begin(), it);
+			while (it != input->end() && (*it) != ' ')
+				it++;
+			*path = input->substr(pos, std::distance(input->begin(), it) - pos).c_str();
+		}
+		else
+			it++;
+		while (it != input->end() && *it == ' ')
+			it++;
+		input->erase(input->begin(), it);
+		it = input->begin();
+	}
 }
 
 int main(int ac, char **av) {
@@ -80,48 +149,7 @@ int main(int ac, char **av) {
 			input.append(" ");
 	}
 	int	ret;
-	std::string::iterator	it;
-	it = input.begin();
-	while (it != input.end() && *it == '-')
-	{
-		if (*(it + 1) == 'r' && *(it + 2) == ' ')
-		{
-			levl = 5;
-			it += 2;
-			if (it + 1  != input.end() 
-					&& *(it + 1) == '-' && it + 2 != input.end()
-					&& *(it + 2) == 'l' && it + 3 != input.end()
-					&& *(it + 3) == ' ' && it + 4 != input.end()
-					&& isdigit(*(it + 4)))
-			{
-				it += 4;
-				levl = 0;
-				while (it != input.end() && isdigit(*it))
-				{
-					int	power = 0;
-					lvl = lvl * pow(10, power)  + (*it) - 48;
-					power++;
-					it++;
-				}
-			}
-		}
-		else if (it + 1 != input.end() && *(it + 1) == 'p'
-				&& it + 2 != input.end())
-		{
-			size_t	pos;
-			it += 3;
-			pos = std::distance(input.begin(), it);
-			while (it != input.end() && (*it) != ' ')
-				it++;
-			path = input.substr(pos, std::distance(input.begin(), it) - pos).c_str();
-		}
-		else
-			it++;
-		while (it != input.end() && *it == ' ')
-			it++;
-		input.erase(input.begin(), it);
-		it = input.begin();
-	}
+	options_handler(&input, &path);
 	if (input.empty() == false && (ret = link_handler(input.c_str(), lvl)) < 0)
 		return (ret);
 	if (img_request(path.c_str()))
